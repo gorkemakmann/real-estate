b2b14f498c46ffed5f5a4056c6cee5999487e318
Görkem <gorkemakmann@gmail.com>
2024-02-13 16:19:12 +0300

    Create server and connect to database

---
+++ API/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.DB_CONNECTION_URI).then(() => {
+    console.log('Connected to DB');
+}).catch((err) => {
+    console.log(err);
+});
+
+const app = express();
+
+app.listen(3000,() => {
+    console.log('Server is running on port 3000!');
+});
